b1030a601060558fcb2d9bc4ea257369549b1409
ofrimpong <ofrimpong@miners.utep.edu>
2024-10-14 17:00:58 -0600

    debugger

---
+++ debugger.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main() {
+
+ float fahrenheit;
+
+ printf("Enter temperature in Fahrenheit: ");
+
+ scanf("%f", &fahrenheit);
+ float celsius;
+ celsius = (fahrenheit - 32) * 5 / 9;
+
+ printf("Temperature in Celsius: %f\n", celsius);
+
+ return 0;
+
+}
